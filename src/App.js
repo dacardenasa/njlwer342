440ca528eb06add577596b477d7a578079f8dfc9
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-29 11:38:57 -0500

    Add function for create a task in App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      list: ["Sacar la ropa", "Hacer la cama", "Leer un rato"],
+      inputText: ''
+    }
+  }
+
+  handleSubmit(event){
+    this.setState(
+      {
+        list: this.state.list.concat([this.state.inputText]),
+        inputText: ''
+      }
+    );
+    event.preventDefault();
+  }
+
+  handleChange(event){
+    this.setState(
+      {
+        inputText: event.target.value
+      }
+    );
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.list.map((item, index) => 
+              <li key={index}>{item}</li>
+            )}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              id="new-task"
+              placeholder="Ingresa una tarea y oprime Enter"
+              onChange={this.handleChange}
+              value={this.state.inputText}
+            />
+          </form>
         </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
